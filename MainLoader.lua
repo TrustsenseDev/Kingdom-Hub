a4a5f2aeee32a53a2c3b504686619dc822fd815f
Trustsense <98208313+TrustsenseDev@users.noreply.github.com>
2022-02-24 22:01:07 +0200

    Create MainLoader.lua

---
+++ MainLoader.lua
@@ -0,0 +1,13 @@
+local discordinv = "https://discord.gg/mwTwNTbGws"
+local d
+local f = pcall(function()
+    d =
+        game:HttpGet("https://raw.githubusercontent.com/TrustsenseDev/Andromeda-Hub/main/Game%20Supported/" .. game.PlaceId .. ".lua")
+end)
+
+if f == true then
+    loadstring(d)()
+else
+    game.Players.LocalPlayer:Kick("Not Supported | Discord Link Copied To Clipboard" .. discordinv)
+    setclipboard(discordinv)
+end
