138f9a7760bc3c7342fa6a3b9ee287987eda54d3
Trustsense <98208313+TrustsenseDev@users.noreply.github.com>
2022-03-05 18:33:12 +0200

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -22,18 +22,17 @@ local function invite()
     end
 end
 
-local SelectedGame
-
-local discord = "https://discord.gg/mwTwNTbGws"
-local checker = pcall(function()
-    SelectedGame =
-        game:HttpGet("https://raw.githubusercontent.com/TrustsenseDev/Kingdom-Hub/main/Game%20Supported/" .. game.PlaceId .. ".lua")
+local discordinv = "https://discord.gg/mwTwNTbGws"
+local d
+local f = pcall(function()
+    d =
+        game:HttpGet("https://raw.githubusercontent.com/TrustsenseDev/Andromeda-Hub/main/Game%20Supported/" .. game.PlaceId .. ".lua")
 end)
 
-if checker == true then
-    loadstring(SelectedGame)()
+if f == true then
+    loadstring(d)()
 else
-    game.Players.LocalPlayer:Kick("Not Supported | Discord Link Copied To Clipboard " .. discord)
+    game.Players.LocalPlayer:Kick("Not Supported | Discord Link Copied To Clipboard " .. discordinv)
     invite()
-    setclipboard(discord)
+    setclipboard(discordinv)
 end
