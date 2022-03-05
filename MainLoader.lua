079bad5519034bf9c7043ef291fac36064962258
Trustsense <98208313+TrustsenseDev@users.noreply.github.com>
2022-03-05 18:33:45 +0200

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -26,7 +26,7 @@ local discordinv = "https://discord.gg/mwTwNTbGws"
 local d
 local f = pcall(function()
     d =
-        game:HttpGet("https://raw.githubusercontent.com/TrustsenseDev/Andromeda-Hub/main/Game%20Supported/" .. game.PlaceId .. ".lua")
+        game:HttpGet("https://raw.githubusercontent.com/TrustsenseDev/Kingdom-Hub/main/Game%20Supported/" .. game.PlaceId .. ".lua")
 end)
 
 if f == true then
